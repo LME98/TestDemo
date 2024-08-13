7fc14a0f5231b6ca8a1313f88b3111361c77cad4
LME98 <madhurieluri75@gmail.com>
2024-08-12 21:43:51 -0500

    First Commit

---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        char a = 'G';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        double d = 36.77;
+        float f = 18.54f;
+        long l = 12121;
+
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+    }
+}
